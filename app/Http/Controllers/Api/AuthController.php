37e47553ca5c3275bb48c769c1cb1c3209a62fec
Prakash171999 <prr0154@my.londonmet.ac.uk>
2020-02-25 18:33:41 +0545

    API for registration working fine after the modification.(25feb)

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,25 +6,36 @@ use App\Http\Controllers\Controller;
 use App\User;
 use App\Driver;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
    public function register(Request $request)
    {
-        $validatedData = $request->validate([
+        $validatedData = Validator::make($request->all(), [
             'fullname' => 'required|max:60',
             'phoneno' => 'required|min:10',
             'dob' => 'required', 'date',
             'email' => 'email|required|unique:users',
-            'password' => 'required|confirmed',
+            'password' => 'required|string|min:8',
         ]);
-        $user = User::create($validatedData);
-        //generating accesstoken with name authToken
-        $accessToken = $user->createToken('authToken')->accessToken;
-        // returning response containing user object and access token.
-        return response(['user'=>$user, 'access_token'=>$accessToken]);
+        if($validatedData->fails()){
+            return response()->json($validatedData->errors()->toArray());
+        } else {
+            $user = new User();
+            $user->fullname = $request->input('fullname');
+            $user->phoneno = $request->input('phoneno');
+            $user->dob = $request->input('dob');
+            $user->email = $request->input('email');
+            $user->password = Hash::make($request->input('password'));
+            $user->save();
+            //generating accesstoken with name authToken
+            $accessToken = $user->createToken('authToken')->accessToken;
+            // returning response containing user object and access token.
+            return response()->json(['user' => $user,'access_token'=>$accessToken]);
 
-        
+        }        
     }
    public function login(Request $request)
    {
@@ -39,6 +50,10 @@ class AuthController extends Controller
         }
         //when the data is valid generate token
         $accessToken = auth()->user()->createToken('authToken')->accessToken;
-        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+        return response()->json(['user' => auth()->user(), 'access_token' => $accessToken]);
+   }
+
+   public function test(){
+       return response()->json(['foo' => 'bar']);
    }
 }
